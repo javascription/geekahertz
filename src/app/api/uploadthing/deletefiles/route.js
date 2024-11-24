4748b9ba98a87044e2e3e9a704a4decb05784a1b
ShoneUnity <e10789akansh@dpsrkp.net>
2024-11-24 14:43:04 +0530

    istg

--- src/app/api/uploadthing/deletefiles/route.js
+++ src/app/api/uploadthing/deletefiles/route.js
@@ -2,11 +2,19 @@ import { UTApi } from "uploadthing/server";
 
 export const utapi = new UTApi({});
 
-export async function DELETE(request) {
-  const data = await request.json();
-  console.log(data);
-  const newUrl = data.url.substring(data.url.lastIndexOf("/") + 1);
-  await utapi.deleteFiles(newUrl);
+export async function DELETE(req, res) {
+  try {
+    const data = await req.json();
+    console.log(data);
+    const newUrl = data.url.substring(data.url.lastIndexOf("/") + 1);
+    await utapi.deleteFiles(newUrl);
 
-  return Response.json({ message: "ok" });
+    return Response.json({ message: "ok" });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "skill issue" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
